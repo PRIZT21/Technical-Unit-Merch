164c71516e355812ad5806550e446fe0930abd61
Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2026-02-28 18:37:06 +0100

    update product list style and add filter options

--- merchstore/components/ProductList.tsx
+++ merchstore/components/ProductList.tsx
@@ -1,45 +1,96 @@
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
 const products = [
-	{
-		id: 1,
-		name: "Basic Tee",
-		href: "#",
-		imageSrc:
-			"https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
-		imageAlt: "Front of men's Basic Tee in black.",
-		price: "$35",
-		color: "Black",
-	},
-	{
-		id: 2,
-		name: "Basic Tee",
-		href: "#",
-		imageSrc:
-			"https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
-		imageAlt: "Front of men's Basic Tee in white.",
-		price: "$35",
-		color: "Aspen White",
-	},
-	{
-		id: 3,
-		name: "Basic Tee",
-		href: "#",
-		imageSrc:
-			"https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
-		imageAlt: "Front of men's Basic Tee in dark gray.",
-		price: "$35",
-		color: "Charcoal",
-	},
-	{
-		id: 4,
-		name: "Artwork Tee",
-		href: "#",
-		imageSrc:
-			"https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
-		imageAlt:
-			"Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
-		price: "$35",
-		color: "Iso Dots",
-	},
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "₦5,500",
+    color: "Black",
+    size: "M",
+  },
+  {
+    id: 2,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
+    imageAlt: "Front of men's Basic Tee in white.",
+    price: "₦5,800",
+    color: "Aspen White",
+    size: "M",
+  },
+  {
+    id: 3,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
+    imageAlt: "Front of men's Basic Tee in dark gray.",
+    price: "₦6,000",
+    color: "Charcoal",
+    size: "M",
+  },
+  {
+    id: 4,
+    name: "Artwork Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
+    imageAlt:
+      "Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
+    price: "₦6,500",
+    color: "Iso Dots",
+    size: "M",
+  },
+  {
+    id: 5,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "₦5,700",
+    color: "Black",
+    size: "L",
+  },
+  {
+    id: 6,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
+    imageAlt: "Front of men's Basic Tee in white.",
+    price: "₦6,200",
+    color: "Aspen White",
+    size: "L",
+  },
+  {
+    id: 7,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
+    imageAlt: "Front of men's Basic Tee in dark gray.",
+    price: "₦6,800",
+    color: "Charcoal",
+    size: "L",
+  },
+  {
+    id: 8,
+    name: "Artwork Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
+    imageAlt:
+      "Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
+    price: "₦7,000",
+    color: "Iso Dots",
+    size: "L",
+  },
 ];
 
 export default function ProductOverview() {
@@ -56,18 +107,19 @@ export default function ProductOverview() {
 							/>
 							<div className="mt-4 flex justify-between">
 								<div>
-									<h3 className="text-sm text-gray-700">
+									<h3 className="text-2xl font-bold">
 										<a href={product.href}>
 											<span aria-hidden="true" className="absolute inset-0" />
 											{product.name}
 										</a>
 									</h3>
-									<p className="mt-1 text-sm text-gray-500">{product.color}</p>
+									<p className="mt-1 text-sm text-gray-500">
+										{product.color}, {product.size}
+									</p>
 								</div>
-								<p className="text-sm font-medium text-gray-900">
-									{product.price}
-								</p>
+								<p className="text-lg font-semibold self-center">{product.price}</p>
 							</div>
+								<Button className="w-full mt-8 rounded-sm" content="leftIcon" leftIcon={<ShoppingCart />}>Add to Cart</Button>
 						</div>
 					))}
 				</div>
--- merchstore/components/home/CategoriesSection.tsx
+++ merchstore/components/home/CategoriesSection.tsx
@@ -1,17 +1,41 @@
 import ProductList from "@/components/ProductList";
-
+import { Button } from "@/components/ui/button";
+import { Handbag, Palette, Scaling } from "lucide-react";
 
 export default function CategoriesSection() {
-  return (
-    <>
-      <div className="flex justify-between items-start mt-16 mx-32">
-        <div>
-          <h3 className="text-3xl font-bold">Category</h3>
-        </div>
-        <div>
-          <ProductList/>
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="flex justify-between items-start mt-16 mx-32">
+				<div>
+					<h3 className="text-3xl font-bold">Category</h3>
+					<div>
+						<Button
+							content="leftIcon"
+							leftIcon={<Handbag />}
+							className="mt-4 w-1/2 rounded-lg opacity-60"
+						>
+							All Categories
+						</Button>{" "}
+						<Button
+							content="leftIcon"
+							leftIcon={<Palette />}
+							className="mt-4 w-1/2 rounded-lg opacity-60"
+						>
+							Color
+						</Button>{" "}
+						<Button
+							content="leftIcon"
+							leftIcon={<Scaling />}
+							className="mt-4 w-1/2 rounded-lg opacity-60"
+						>
+							Size
+						</Button>{" "}
+					</div>
+				</div>
+				<div>
+					<ProductList />
+				</div>
+			</div>
+		</>
+	);
 }
