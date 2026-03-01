f0415dc480dd9b826acf84e65f20df465ecc0dc6
Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2026-03-01 20:45:44 +0100

    update data

---
+++ merchstore/lib/products.ts
@@ -0,0 +1,63 @@
+export const products = [
+  {
+    id: "hoodie",
+    name: "Light Bearers Hoodie",
+    price: "₦5,500",
+    href: "#",
+    variants: [
+      {
+        id: "hoodie-black",
+        color: "Black",
+        imageSrc:
+          "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
+        imageAlt: "Light Bearers Hoodie in Black",
+        sizes: ["S", "M", "L", "XL"],
+      },
+      {
+        id: "hoodie-white",
+        color: "White",
+        imageSrc:
+          "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
+        imageAlt: "Light Bearers Hoodie in White",
+        sizes: ["S", "M", "L", "XL"],
+      },
+    ]
+  },
+  {
+    id: "tshirt",
+    name: "Classic T-Shirt",
+    price: "₦2,500",
+    href: "#",
+    variants: [
+      {
+        id: "tshirt-black",
+        color: "Black",
+        imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
+        imageAlt: "Classic T-Shirt in Black",
+        sizes: ["S", "M", "L", "XL", "XXL"],
+      },
+      {
+        id: "tshirt-navy",
+        color: "Navy",
+        imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
+        imageAlt: "Classic T-Shirt in Navy",
+        sizes: ["S", "M", "L", "XL"],
+      },
+    ]
+  },
+  {
+    id: "cap",
+    name: "Signature Cap",
+    price: "₦1,800",
+    href: "#",
+    variants: [
+      {
+        id: "cap-black",
+        color: "Black",
+        imageSrc: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
+        imageAlt: "Signature Cap in Black",
+        sizes: ["One Size"],
+      },
+    ]
+  }
+];
