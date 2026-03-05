325e0aa0aebf6cf7b7504e4dfe91eaaab7c2f7d1
Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2026-03-05 12:31:56 +0100

    add sonner package from shadcnui

--- merchstore/app/layout.tsx
+++ merchstore/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '../components/ui/Header'
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         <Header />
         {children}
+        <Toaster/>
       </body>
     </html>
   )
