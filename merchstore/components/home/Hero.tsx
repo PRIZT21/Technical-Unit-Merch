d2d79bc13d07a415e8660de1decce864f63cd743
Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2026-03-06 12:36:02 +0100

    update hero section responsiveness

--- merchstore/components/home/Hero.tsx
+++ merchstore/components/home/Hero.tsx
@@ -4,12 +4,13 @@ import CTASection from "@/components/home/CTASection";
 export default function Hero() {
 	return (
 		<div>
-			<div className="relative isolate px-6 pt-14 lg:px-8 h-screen flex items-end justify-center bg-[url(/images/hero_image.webp)] bg-cover bg-top">
+			<div className="relative isolate px-6 pt-14 lg:px-8 h-[75vh] md:h-screen flex items-end justify-center bg-[url(/images/hero_image.webp)] bg-cover bg-top">
 				<Image
 					src="/images/Light_Bearers_Theme.webp"
 					alt="Light Bearers Theme"
 					height={600}
 					width={600}
+					className="mb-16 md:mb-2"
 				/>
 			</div>
 			<CTASection />
