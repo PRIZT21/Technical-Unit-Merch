e4c36c0f2a6d1564cde5c8700c4153f21021926f
Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2026-03-11 16:12:28 +0100

    style: format code for consistency and readability in layout component

--- merchstore/app/layout.tsx
+++ merchstore/app/layout.tsx
@@ -1,29 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Toaster } from "@/components/ui/sonner"
-import ConditionalHeader from "@/components/ui/ConditionalHeader"
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
+import ConditionalHeader from "@/components/ui/ConditionalHeader";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Technical Unit Merch Store',
-  description: 'A simple online store for Technical Unit merchandise.',
-}
+	title: "Technical Unit Merch Store",
+	description: "A simple online store for Technical Unit merchandise.",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-     
-      <body className={`${inter.className}`}>
-        <ConditionalHeader />
-        {children}
-        <Toaster/>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={`${inter.className}`}>
+				<ConditionalHeader />
+				{children}
+				<Toaster />
+			</body>
+		</html>
+	);
 }
